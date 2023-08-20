b3e28c78e919a8650b5314ebcfd33bd588cedd58
andrew-matt <andrew.matt04@gmail.com>
2023-08-20 17:07:49 +0300

    add navigation

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
 import style from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav className={style.nav}>
       <div className={style.item}>
-        <a href="/profile">Profile</a>
-      </div>
-      <div className={`${style.item} ${style.active}`}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/profile" activeClassName={style.active}>Profile</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/news">News</a>
+        <NavLink to="/dialogs" activeClassName={style.active}>Messages</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/news" activeClassName={style.active}>News</NavLink>
       </div>
       <div className={style.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/music" activeClassName={style.active}>Music</NavLink>
+      </div>
+      <div className={style.item}>
+        <NavLink to="/settings" activeClassName={style.active}>Settings</NavLink>
       </div>
     </nav>
   );
