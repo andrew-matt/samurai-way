c0dcb3e72707fa4f3c53d0d8604187c6fb4959cd
andrew-matt <andrew.matt04@gmail.com>
2023-08-20 17:22:23 +0300

    add messages

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import style from './Dialogs.module.css';
+
 export const Dialogs = () => {
   return (
-    <div>
-      Dialogs
+    <div className={style.dialogs}>
+      <div className={style.dialogsItems}>
+        <div className={style.dialog}>John</div>
+        <div className={style.dialog}>Bob</div>
+        <div className={style.dialog}>Ann</div>
+        <div className={style.dialog}>Helen</div>
+        <div className={style.dialog}>Alice</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>Hi</div>
+        <div className={style.message}>Good day</div>
+        <div className={style.message}>Hello</div>
+      </div>
     </div>
   );
 };
